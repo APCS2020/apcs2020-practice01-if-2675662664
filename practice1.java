695fe63440635035a8d6baede3307dcb1dd222ca
Jason金正浩 <50470279+2675662664@users.noreply.github.com>
2019-05-17 11:05:50 +0800

    homework

---
+++ practice1.java
@@ -0,0 +1,16 @@
+public class jzh{
+    public static void main(String[] args) {
+        int age =20;
+        int user =23;
+        boolean is = false;
+        if (user>= age){
+            System.out.println("I like apple!");
+        }
+        else{
+            System.out.println("I not like apple!");
+        }
+        if (is){
+            System.out.println("you like apple!");
+        }
+    }
+}
